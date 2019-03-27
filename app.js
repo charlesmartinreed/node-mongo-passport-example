230bd7220c5545ddfcbd3623cc04e9e5b6e4b81d
charlesmartinreed <charlesmartinreed@icloud.com>
2019-03-27 18:29:04 -0500

    Add mongoose. Connect app to Atlas cluster.

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ const express = require('express'),
 			app = express(),
 			port = process.env.PORT || 5000;
 
+const mongoose = require('mongoose');
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// connect to mongo
+mongoose.connect(db, { useNewUrlParser: true })
+	.then(() => console.log('MongoDB Connected...'))
+	.catch(err => console.log(err));
+
 // EJS Middleware
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+	mongoURI: 'mongodb+srv://charlie-rwgod:2-8deG06@demo-cluster-bfnxp.mongodb.net/test?retryWrites=true'
+}
